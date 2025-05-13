49529cb04c8d130a97285395948fd1a6e688b073
Starnsworth <starns@starnsworth.com>
2025-05-13 16:30:09 +1000

    initial commit

---
+++ index.py
@@ -0,0 +1,16 @@
+#    Program to guess a number from 1 to 5.
+#    Program to guess a number from 1 to 100 in least tries using higher or lower messages to the user.
+#    Paper/rock/scissors in Python
+#    MD5 hash a password entered by the user.
+#    Program to ping a port to see if it is open.
+#    Ping a port range on an IP.
+#    Password checker that does not permit duplicate characters together. These passwords will be detected and blocked. ["0000000000", "aabbccddeeffgg", "abc123@#$$"]
+#    Password checker that detects and blocks passwords with an increasing or decreasing sequence of 3 characters or more. Block passwords such as: ["1234567", "abc12"]
+#    Password checker that detects and blocks passwords from a list even if the banned sequence is embedded in a larger user supplied password.. Block passwords such as: ["qwerty", "QWERTY", "asdfg", "!@#$%^&*()"]
+#    A strong password needs any 2 of the following rules and 12 characters. One upper case, one lower case, one number, one symbol.
+#    A password must have 10 characters and meet all 4 basic rules of One upper case, one lower case, one number, one symbol. Alternatively it can have 14 characters and only 3 lowers, 3 uppers and one from either symbols or numbers.
+#    Create a program to encrypt a text file using a password specified by the user and 128 bit encryption. Prove that it encrypts and decrypts.
+#    Create a program to encrypt a text file using a salted key in a file supplied by the user. Use AES256 encryption.
+#    Try out TKinter to add a user interface to the password checker.
+#    Try out Django to make a web page to check a password supplied by the user.
+#    Program to find the password used to generate a MD5 hash supplied by the user.
